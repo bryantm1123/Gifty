5109520d8c07461e6588d8ecaa28db55fe807d45
Matt Bryant <bryantm1122@gmail.com>
2021-06-11 22:08:21 -0400

    initial commit adding scaffolding for feed collection view added cell to hold our gifs

---
+++ Gifty/Feed/Views/FeedViewController.swift
@@ -0,0 +1,93 @@
+//
+//  FeedViewController.swift
+//  Gifty
+//
+//  Created by Matt Bryant on 6/11/21.
+//
+
+import UIKit
+
+class FeedViewController: UICollectionViewController {
+    
+    private let reuseIdentifier = "GifCell"
+    private let sectionInsets = UIEdgeInsets(
+      top: 50.0,
+      left: 20.0,
+      bottom: 50.0,
+      right: 20.0)
+    private let itemsPerRow: CGFloat = 2
+    
+    let items: [Int] = [1,2,3,4,5,6,7,9,10]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+// MARK: DataSource
+extension FeedViewController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+      }
+      
+      // 2
+      override func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+      ) -> Int {
+        return items.count
+      }
+      
+      // 3
+      override func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+      ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+          withReuseIdentifier: reuseIdentifier,
+          for: indexPath)
+        cell.backgroundColor = .black
+        // Configure the cell
+        return cell
+      }
+}
+
+// MARK:
+extension FeedViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+      _ collectionView: UICollectionView,
+      layout collectionViewLayout: UICollectionViewLayout,
+      sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+      let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+      let availableWidth = view.frame.width - paddingSpace
+      let widthPerItem = availableWidth / itemsPerRow
+      
+      return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(
+      _ collectionView: UICollectionView,
+      layout collectionViewLayout: UICollectionViewLayout,
+      insetForSectionAt section: Int
+    ) -> UIEdgeInsets {
+      return sectionInsets
+    }
+    
+    func collectionView(
+      _ collectionView: UICollectionView,
+      layout collectionViewLayout: UICollectionViewLayout,
+      minimumLineSpacingForSectionAt section: Int
+    ) -> CGFloat {
+      return sectionInsets.left
+    }
+}
---
+++ Gifty/Feed/Views/GifCell.swift
@@ -0,0 +1,12 @@
+//
+//  GifCell.swift
+//  Gifty
+//
+//  Created by Matt Bryant on 6/11/21.
+//
+
+import UIKit
+
+class GifCell: UICollectionViewCell {
+    
+}
